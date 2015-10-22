8fd1c98f6f036e39098d5c43e60b1e34be1ee8bf
speedyGonzales <yonchoy@abv.bg>
2015-10-22 15:41:29 +0300

    Update example_test.go
    added some simple tests

--- tasks/01/example_test.go
+++ tasks/01/example_test.go
@@ -21,3 +21,39 @@ func TestExtractingIPs(t *testing.T) {
 		t.Errorf("Expected\n---\n%s\n---\nbut found\n---\n%s\n---\n", expected, found)
 	}
 }
+
+func TestExtractingDates(t *testing.T) {
+	logContents := `2015-08-23 12:37:03 8.8.8.8 As far as we can tell this is a DNS
+2015-08-23 12:37:04 8.8.4.4 Yet another DNS, how quaint!
+2015-08-23 12:37:05 208.122.23.23 There is definitely some trend here
+`
+
+	expected := `2015-08-23 12:37:03
+2015-08-23 12:37:04
+2015-08-23 12:37:05
+`
+
+	found := ExtractColumn(logContents, 0)
+
+	if found != expected {
+		t.Errorf("Expected\n---\n%s\n---\nbut found\n---\n%s\n---\n", expected, found)
+	}
+}
+
+func TestExtractingTexts(t *testing.T) {
+	logContents := `2015-08-23 12:37:03 8.8.8.8 As far as we can tell this is a DNS
+2015-08-23 12:37:04 8.8.4.4 Yet another DNS, how quaint!
+2015-08-23 12:37:05 208.122.23.23 There is definitely some trend here
+`
+
+	expected := `As far as we can tell this is a DNS
+Yet another DNS, how quaint!
+There is definitely some trend here
+`
+
+	found := ExtractColumn(logContents, 2)
+
+	if found != expected {
+		t.Errorf("Expected\n---\n%s\n---\nbut found\n---\n%s\n---\n", expected, found)
+	}
+}
